9ec4ed0d3c4c77b7b19526c90919b3f4e07a4780
Van Duy <motthoihoctro98@gmail.com>
2020-01-02 19:24:04 +0700

    push 2/1/2020

---
+++ 394/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#banner").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true
+    });
+});
